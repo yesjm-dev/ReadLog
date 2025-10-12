af175ce4f59e921e31dd04d758b7cdd030c0dfd7
노정민 <48047620+yesjm-dev@users.noreply.github.com>
2025-10-13 01:10:05 +0900

    feat: rating jvminline 적용

--- src/main/kotlin/com/yesjm/readlog/domain/model/Rating.kt
+++ src/main/kotlin/com/yesjm/readlog/domain/model/Rating.kt
@@ -2,7 +2,8 @@ package com.yesjm.readlog.domain.model
 
 import com.yesjm.readlog.domain.exception.InvalidRatingException
 
-class Rating(val value: Int) {
+@JvmInline
+value class Rating(val value: Int) {
     init {
         if (value !in 1..5) {
             throw InvalidRatingException("평점은 1에서 5 사이여야 합니다. 현재: $value")
